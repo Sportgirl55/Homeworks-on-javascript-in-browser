4c0799037a638d628f23f2eecfcfc04d4d09ccc6
Elena Slyusareva <sportgirl55@bk.ru>
2020-10-07 17:41:45 +0500

    First task is done

---
+++ js-features/countdown/task.js
@@ -0,0 +1,9 @@
+"use strict";
+let counter = setInterval(function () {
+     let timer = document.getElementById("timer");
+     timer.textContent--;    
+     if (timer.textContent == 0) { 
+          alert("Вы победили в конкурсе!");
+          clearInterval(counter);
+     }
+}, 1000);
