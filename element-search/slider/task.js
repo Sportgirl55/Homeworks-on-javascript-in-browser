9d46b0b59ab724b0e7b1e516e64707fd93b41a67
Elena Slyusareva <sportgirl55@bk.ru>
2020-10-13 19:32:49 +0500

    All tasks completed

---
+++ element-search/slider/task.js
@@ -0,0 +1,27 @@
+let prevArrow = document.querySelector('.slider__arrow_prev');
+let nextArrow = document.querySelector('.slider__arrow_next');
+let slider = document.querySelectorAll('.slider__item');
+let i = 0;
+nextArrow.addEventListener('click', function () {
+     i++;
+     if (i >= slider.length) {
+          slider[i - 1].classList.remove('slider__item_active');
+          i = 0;
+          slider[i].classList.add('slider__item_active');
+     } else {
+          slider[i - 1].classList.remove('slider__item_active');
+          slider[i].classList.add('slider__item_active');
+     }
+})
+
+prevArrow.addEventListener('click', function () {
+     i--;
+     if(i <= 0) {
+          slider[i + 1].classList.remove('slider__item_active');
+          i = slider.length - 1;
+          slider[i].classList.add('slider__item_active');
+     } else {
+          slider[i + 1].classList.remove('slider__item_active');
+          slider[i].classList.add('slider__item_active');
+     }
+})
