17afc2c106b1c4fd049925add3ace9c776dcc53d
Elena Slyusareva <sportgirl55@bk.ru>
2020-10-16 16:53:13 +0500

    All tasks are fixed and rewritten

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -4,11 +4,12 @@ let modalMain = document.getElementById('modal_main');
      modalMain.classList.add('modal_active');
 
 let closedWindow = document.getElementsByClassName('modal__close');
-for (let i = 0; i < closedWindow.length; i++)
+for (let i = 0; i < closedWindow.length; i++) {
      closedWindow[i].onclick = function () {
           this.closest('.modal').classList.remove('modal_active');  
      }
-       
+}
+    
 let success = document.querySelector('.show-success');
 let modalSuccess = document.querySelector('#modal_success');
      success.onclick = function () {
