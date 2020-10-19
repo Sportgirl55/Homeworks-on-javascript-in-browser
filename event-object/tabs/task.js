c5a12dfc257a34b654776eaf32f3836d24f4de09
Elena Slyusareva <sportgirl55@bk.ru>
2020-10-19 21:31:59 +0500

    Removed unnecessary code in the third task

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,17 +3,6 @@
 let tabs = Array.from(document.querySelectorAll('.tab'));
 let tabContent = document.querySelectorAll('.tab__content');
 
-
-// tabs.forEach(function(elem) {
-//      elem.addEventListener('click', function () {
-//           console.log(elem.indexOf('', 0))
-//      })
-// })
-
-
-
-
-
 for(let i = 0; i < tabs.length; i++) {
      tabs[i].addEventListener('click', function () {
           tabs.forEach(function (item, index, arr) {
@@ -26,31 +15,3 @@ for(let i = 0; i < tabs.length; i++) {
                      tabContent[i].classList.add('tab__content_active');
                 })
 }
-
-
-
-
-
-
-
-
-// let button = Array.from(document.querySelectorAll('.dropdown__value')); //кнопка
-// let menu = Array.from(document.querySelectorAll('.dropdown__list')); // список
-// let dropDownItem = Array.from(document.querySelectorAll('.dropdown__item')); // пункты списка
-
-// for(let i = 0; i < button.length; i++) {
-//      button[i].addEventListener('click', function () {
-//           this.nextElementSibling.classList.toggle('dropdown__list_active');
-//      })
-// }
-
-// dropDownItem.forEach(function(elem) {
-//      elem.onclick = function () {
-//           this.closest('.dropdown__list').classList.remove('dropdown__list_active');
-//           this.closest(".dropdown").firstElementChild.textContent = this.textContent;
-//               return false; 
-//      }
-// })
-
-
-
