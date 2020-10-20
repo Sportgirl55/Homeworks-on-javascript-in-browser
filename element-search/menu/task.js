60398a6be235e1bdab26ff3c693179cd014bb0fb
Elena Slyusareva <sportgirl55@bk.ru>
2020-10-20 14:27:42 +0500

    Another version of the second task

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,51 +1,31 @@
 "use strict";
 
-let menuLink = Array.from(document.getElementsByClassName('menu__link'));
-let menuActive = document.getElementsByClassName('menu_active');
-menuLink.forEach(function (elem) {
-     if(elem.nextElementSibling != null) {
-          elem.onclick = function () {
-               closeMenu ();
-               elem.nextElementSibling.classList.toggle('menu_active');
-               if (menuActive[0] !== undefined) {
-                    elem.onclick = function () {
-                         if (this.querySelector('.menu_active')) {
-                              this.classList.remove('menu_active');
-                         }
-                    }
-               }
-               return false;
+let menuLink = Array.from(document.querySelectorAll('.menu__link')); //ссылки в каждом пункте меню
+
+menuLink.forEach((elem) => {
+     elem.onclick = function () {
+          let menuActive = document.querySelector('.menu_active');// активное меню
+          if(menuActive) {
+               if (menuActive !== elem.nextElementSibling) {
+                    menuActive.classList.remove('menu_active');
           }
      }
-})
-
-
-function closeMenu(){
-     for (var i = 0; i < menuActive.length; i++) {
-         menuActive[i].classList.remove('menu_active');
+     elem.nextElementSibling.classList.toggle('menu_active');
+               return false;
      }
-}
- 
-// Первоначальный полурабочий вариант
+}) 
 
-  
+
+
+// // Самый простой рабочий вариант
 // "use strict";
 
-// let menuLink = document.getElementsByClassName('menu__link');
-// let menuActive = document.getElementsByClassName('menu_active');
-
-// for (let i = 0; i < menuLink.length; i++) {
-//      if(menuLink[i].nextElementSibling != null) {
-//           menuLink[i].onclick = function () {
-//                for (let b = 0; b < menuActive.length; b++)  {
-//                     if (menuActive[b] !== undefined) {
-//                          menuActive[b].classList.remove('menu_active');
-//                     }
-//                }
-//                menuLink[i].nextElementSibling.classList.toggle('menu_active');
+// let menuLink = Array.from(document.getElementsByClassName('menu__link'));
+// menuLink.forEach(function(elem) {
+//      elem.onclick = function () {
+//           if(elem.nextElementSibling != null) {
+//                elem.nextElementSibling.classList.toggle('menu_active');
 //                return false;
-//           }          
+//           }      
 //      }
-// }
-
-
+// }) 
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,21 +1,42 @@
 "use strict";
 let prevArrow = document.querySelector('.slider__arrow_prev');
 let nextArrow = document.querySelector('.slider__arrow_next');
-let slider = Array.from(document.querySelectorAll('.slider__item'));
-let i = 0;
+let slides = Array.from(document.querySelectorAll('.slider__item'));
+let index = slides.findIndex(slide => slide.classList.contains('slider__item_active')); 
 nextArrow.addEventListener('click', function () {
-     i++;
-     // i <= slider.length ? slider[i - 1].classList.remove('slider__item_active') :  i = 0;
-     if (i <= slider.length) {
-          slider[i - 1].classList.remove('slider__item_active');
-     } if (i >= slider.length) {
-           i = 0;
-     } 
-     slider[i].classList.add('slider__item_active'); 
+     slides[index].classList.remove('slider__item_active'); 
+     index = index === slides.length - 1 ? 0 : index + 1; 
+     slides[index].classList.add('slider__item_active'); 
 });
 
 prevArrow.addEventListener('click', function () {
-     i--;
-     i > 0 ? slider[i + 1].classList.remove('slider__item_active') : i = slider.length - 1;
-     slider[i].classList.add('slider__item_active');
+     slides[index].classList.remove('slider__item_active'); 
+     index = index === 0 ? slides.length - 1 : index - 1; 
+     slides[index].classList.add('slider__item_active'); 
 });
+
+
+// рабочая версия 
+
+// "use strict";
+// let prevArrow = document.querySelector('.slider__arrow_prev');
+// let nextArrow = document.querySelector('.slider__arrow_next');
+// let slider = Array.from(document.querySelectorAll('.slider__item'));
+// let i = 0;
+// nextArrow.addEventListener('click', function () {
+//      i++;
+//      // i <= slider.length ? slider[i - 1].classList.remove('slider__item_active') :  i = 0;
+//      if (i <= slider.length) {
+//           slider[i - 1].classList.remove('slider__item_active');
+//      } if (i >= slider.length) {
+//            i = 0;
+//      } 
+//      slider[i].classList.add('slider__item_active'); 
+// });
+
+// prevArrow.addEventListener('click', function () {
+//      i--;
+//      i > 0 ? slider[i + 1].classList.remove('slider__item_active') : i = slider.length - 1;
+//      slider[i].classList.add('slider__item_active');
+// });
+
