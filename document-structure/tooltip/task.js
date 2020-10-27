598ec5f8ca3bfb4bc912208ea5d29289e8199c68
Elena Slyusareva <sportgirl55@bk.ru>
2020-10-27 15:01:58 +0500

    First task is done

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let phrases = document.querySelectorAll('.has-tooltip');
+let tooltip = document.createElement('div');
+tooltip.classList.add('tooltip');
+
+for (let phrase of phrases)
+phrase.addEventListener('click', function (e) {
+     phrase.appendChild(tooltip);
+     tooltip.style.top = this.style.top;
+     tooltip.style.left = `${this.getBoundingClientRect().left}px`;
+     tooltip.innerText = e.target.title;
+     tooltip.classList.add('tooltip_active');
+     e.preventDefault();
+})
