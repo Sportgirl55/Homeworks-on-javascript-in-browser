3b1a17cb14c44ea860936663a19f46cc037f2556
Elena Slyusareva <sportgirl55@bk.ru>
2020-11-01 13:51:28 +0500

    Added method .find in the cart

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -13,14 +13,14 @@ for (let i = 0; i < addButtons.length; i++) {
      productButtons[i].querySelector('.product__quantity-control_dec').addEventListener('click', function() {
           quantity[i].innerText > 1 ? quantity[i].innerText-- : quantity[i].innerText = 1;
      })
+
      addButtons[i].addEventListener('click', function ()  {
-          for (let product of productsInCart) {
-               if (product.dataset.id == productButtons[i].dataset.id) {
+          let product = Array.from(productsInCart).find(item => item.dataset.id == productButtons[i].dataset.id);
+          if(product !== undefined) {
                     let cartProductCount = product.querySelector('.cart__product-count').textContent;
                     let totalCount = +cartProductCount + +quantity[i].textContent;
                     return product.querySelector('.cart__product-count').textContent = totalCount;
                 }
-          }
           cart.insertAdjacentHTML('beforeend', `
                             <div class="cart__product" data-id="${productButtons[i].dataset.id}">
                                 <img class="cart__product-image" src="${productButtons[i].querySelector('img').getAttribute('src')}">
@@ -29,3 +29,38 @@ for (let i = 0; i < addButtons.length; i++) {
                             `);    
      })
 }
+
+
+// Рабочий вариант
+
+// 'use strict';
+
+// let productButtons = Array.from(document.querySelectorAll('.product'));
+// let cart = document.querySelector('.cart__products');
+// let productsInCart = cart.children;
+// let addButtons = document.getElementsByClassName('product__add');
+// let quantity = document.querySelectorAll('.product__quantity-value');
+
+// for (let i = 0; i < addButtons.length; i++) { 
+//      productButtons[i].querySelector('.product__quantity-control_inc').addEventListener('click', function() {
+//           quantity[i].innerText++;
+//      })
+//      productButtons[i].querySelector('.product__quantity-control_dec').addEventListener('click', function() {
+//           quantity[i].innerText > 1 ? quantity[i].innerText-- : quantity[i].innerText = 1;
+//      })
+//      addButtons[i].addEventListener('click', function ()  {
+//           for (let product of productsInCart) {
+//                if (product.dataset.id == productButtons[i].dataset.id) {
+//                     let cartProductCount = product.querySelector('.cart__product-count').textContent;
+//                     let totalCount = +cartProductCount + +quantity[i].textContent;
+//                     return product.querySelector('.cart__product-count').textContent = totalCount;
+//                 }
+//           }
+//           cart.insertAdjacentHTML('beforeend', `
+//                             <div class="cart__product" data-id="${productButtons[i].dataset.id}">
+//                                 <img class="cart__product-image" src="${productButtons[i].querySelector('img').getAttribute('src')}">
+//                                 <div class="cart__product-count">${quantity[i].innerText}</div>
+//                             </div>
+//                             `);    
+//      })
+// }
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -7,9 +7,10 @@ tooltip.classList.add('tooltip');
 for (let phrase of phrases)
 phrase.addEventListener('click', function (e) {
      phrase.appendChild(tooltip);
+     tooltip.contains(document.querySelector('.tooltip_active')) && document.querySelector('.tooltip_active').innerText == e.target.title ? 
+     tooltip.classList.remove('tooltip_active') : tooltip.classList.add('tooltip_active');
      tooltip.style.top = this.style.top;
      tooltip.style.left = `${this.getBoundingClientRect().left}px`;
      tooltip.innerText = e.target.title;
-     tooltip.classList.toggle('tooltip_active');
      e.preventDefault();
 })
